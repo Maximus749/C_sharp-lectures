55695f695a104d20d39b4266c2cb48359f26ed47
Maximus749 <maximus749@gmail.com>
2022-11-21 21:54:34 +0300

    Add Example005_If-Else

---
+++ Example005_If-Else/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+if  (username.ToLower() == "маша"){
+    Console.WriteLine("Ура, это же Маша!");
+}
+else{
+   Console.WriteLine("Привет " + username);
+}
