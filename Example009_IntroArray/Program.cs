f64727f4b9446986d30e51096d1e33d4a153043c
Maximus749 <maximus749@gmail.com>
2022-11-27 09:44:03 +0300

    Added Example008_IntroMetod Example009_IntroArray Example010_IntroMethodArray Example011_ArrayLibrary

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,11 @@
+﻿int Max(int arg1, int arg2, int arg3){
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return(result);
+}
+
+int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+array[0] = 12;
+Console.WriteLine(array[0]);
+
